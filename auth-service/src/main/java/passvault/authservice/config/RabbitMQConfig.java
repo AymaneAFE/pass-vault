c4d66788f215148622a79cc71506f7f20c8325e0
Aymane <fajr.pn@gmail.com>
2025-12-28 16:50:31 +0100

    eureka conf in app.properties

---
+++ auth-service/src/main/java/passvault/authservice/config/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package passvault.authservice.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String USER_REGISTERED_QUEUE = "user.registered";
+    public static final String PASSWORD_CHANGED_QUEUE = "password.changed";
+
+    @Bean
+    public Queue userRegisteredQueue() {
+        return new Queue(USER_REGISTERED_QUEUE, true);
+    }
+
+    @Bean
+    public Queue passwordChangedQueue() {
+        return new Queue(PASSWORD_CHANGED_QUEUE, true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
